c7b8145e90082ab45c28e8585da0e45c19ac2998
Merlin Stemmer <stemmermerlin@gmail.com>
2023-02-28 22:22:04 +1300

    Update rooms.supportOtherRooms.ts

--- src/Rooms/rooms.supportOtherRooms.ts
+++ src/Rooms/rooms.supportOtherRooms.ts
@@ -1,11 +1,22 @@
 function supportOtherRooms(room) {
 
-    if(Game.cpu.bucket > 8000 &&
+    if(Game.cpu.bucket > 7000 &&
         Game.time % 100 == 0 &&
         Memory.target_colonise &&
         Game.rooms[Memory.target_colonise.room] &&
-        Game.rooms[Memory.target_colonise.room].controller &&
-        Game.rooms[Memory.target_colonise.room].controller.level >= 3 &&
+        Game.rooms[Memory.target_colonise.room].controller
+
+        &&
+        (
+            Game.rooms[Memory.target_colonise.room].controller.level >= 3 ||
+            Game.rooms[Memory.target_colonise.room].controller.level == 2 &&
+            Game.rooms[Memory.target_colonise.room].memory.Structures &&
+            Game.rooms[Memory.target_colonise.room].memory.Structures.bin &&
+            Game.getObjectById(Game.rooms[Memory.target_colonise.room].memory.Structures.bin)
+        )
+        &&
+
+
         Game.rooms[Memory.target_colonise.room].controller.level <= 5 &&
         room.controller.level == 8 &&
         Game.map.getRoomLinearDistance(room.name, Memory.target_colonise.room) <= 7) {
