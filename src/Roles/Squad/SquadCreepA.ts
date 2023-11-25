f29b9003ae54bbcacb3792554145be1e59341b6b
Merlin Stemmer <stemmermerlin@gmail.com>
2023-11-25 15:02:42 +1300

    Update SquadCreepA.ts

--- src/Roles/Squad/SquadCreepA.ts
+++ src/Roles/Squad/SquadCreepA.ts
@@ -400,7 +400,7 @@ import {roomCallbackSquadA, roomCallbackSquadASwampCostSame, roomCallbackSquadGe
 
                         console.log("heal power is", HealPower, "tower power is", totalTowerDamage);
 
-                        if(totalTowerDamage > HealPower && lowest < creep.hitsMax && target && target.hits < target.hitsMax || target && target.hits <= target.hitsMax/2.1 || enemyCreepInRangeThree.length && enemyCreepInRangeThree.filter(ecreep => ecreep.getActiveBodyparts(ATTACK) > 24 && ecreep.pos.findPathTo(creep, { ignoreCreeps: false, ignoreRoads: true, swampCost: 1 }).length <= 3).length) {
+                        if(totalTowerDamage > HealPower && lowest < creep.hitsMax && target && target.hits < target.hitsMax || target && target.hits <= target.hitsMax/2.1 || enemyCreepInRangeThree.length && enemyCreepInRangeThree.filter(ecreep => ecreep.getActiveBodyparts(ATTACK) > 24 && ecreep.pos.findPathTo(creep, { ignoreCreeps: false, ignoreRoads: true, swampCost: 1 }).length < 3).length) {
 
                             let distance = creep.pos.getRangeTo(closestTower);
 
@@ -413,6 +413,9 @@ import {roomCallbackSquadA, roomCallbackSquadASwampCostSame, roomCallbackSquadGe
                                 }
                             );
                             move_location = fleeTowerPath.path[fleeTowerPath.path.length - 1];
+                        }
+                        else if(enemyCreepInRangeThree.length && enemyCreepInRangeThree.filter(ecreep => ecreep.getActiveBodyparts(ATTACK) > 24 && ecreep.pos.findPathTo(creep, { ignoreCreeps: false, ignoreRoads: true, swampCost: 1 }).length === 3).length) {
+                            move_location = creep.pos;
                         }
                     }
                 }
